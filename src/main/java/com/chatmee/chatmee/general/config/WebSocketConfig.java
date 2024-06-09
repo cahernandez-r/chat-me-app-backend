9d1c28740de7283d2f16a59ae86858eaaeddaff5
camilohr.dev <chernandez@unimayor.edu.co>
2024-06-09 18:04:48 -0500

    Refactor config websocket

--- src/main/java/com/chatmee/chatmee/general/config/WebSocketConfig.java
+++ src/main/java/com/chatmee/chatmee/general/config/WebSocketConfig.java
@@ -14,19 +14,20 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 import java.util.List;
 
 @Configuration
-@EnableWebSocketMessageBroker
+@EnableWebSocketMessageBroker//Enable sub-protocol STOMP (Simple Text Oriented Messaging Protocol)-WebSocket Works as a message broker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/user");
         registry.setApplicationDestinationPrefixes("/app");
+        //registry.enableSimpleBroker("/topic", "/queue");
         registry.setUserDestinationPrefix("/user");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .withSockJS();
+                .setAllowedOrigins("*");
     }
 
     @Override
